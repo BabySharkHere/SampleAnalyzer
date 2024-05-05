c79a1180611f00005bd67d1688e977f1b71080e6
BabySharkHere <82753628+BabySharkHere@users.noreply.github.com>
2024-05-05 10:50:57 +0300

    Create empiricFunction.h

---
+++ src/empiricFunction.h
@@ -0,0 +1,13 @@
+#ifndef empiricFunction_hpp
+#define empiricFunction_hpp
+
+#include <stdio.h>
+#include <string>
+
+#endif /* empiricFunction_hpp */
+#define N 100
+#define M 9
+
+
+void empiricFunction(double matrix[N][M], int numberOfUnique);
+void empericFunctionToFile(double matrix[N][M], int numberOfUnique, std::string fileName);
